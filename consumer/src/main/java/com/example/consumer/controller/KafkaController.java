2927f69ae049b399b0cb57a181520b01c6f8dcb3
Ilya <ilyxa12344321@gmail.com>
2024-02-04 14:49:54 +0400

    Add cors origin for frontend instant

--- consumer/src/main/java/com/example/consumer/controller/KafkaController.java
+++ consumer/src/main/java/com/example/consumer/controller/KafkaController.java
@@ -1,12 +1,27 @@
 package com.example.consumer.controller;
 
+import com.example.consumer.model.Message;
 import com.example.consumer.service.KafkaService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class KafkaController {
 
+    private final KafkaService kafkaService;
+
+    public KafkaController(KafkaService kafkaService) {
+        this.kafkaService = kafkaService;
+    }
+
+    @CrossOrigin(origins = "http://localhost:8080")
+    @GetMapping("get-all")
+    public List<Message> publishMessage() {
+       return kafkaService.getMessages();
+    }
 }
--- producer/src/main/java/com/example/producer/controller/KafkaController.java
+++ producer/src/main/java/com/example/producer/controller/KafkaController.java
@@ -1,6 +1,7 @@
 package com.example.producer.controller;
 
 import com.example.producer.service.KafkaService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class KafkaController {
         this.kafkaService = kafkaService;
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping
     public String publishMessage(String msg) {
         try {
